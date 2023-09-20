beb472527885375a53baeb9c2793f8e5eb259a97
St1cker1 <144201961+St1cker1@users.noreply.github.com>
2023-09-20 11:18:13 +0500

    Update lab1.py апдейт

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,4 +1,4 @@
-import math
-
-def task1():
-  pass
+from math import *  #импортировал математику
+x, y, z = float(input()), float(input()), float(input())
+a = ln(abs(x-1)) - y**(1./3.)/x**(1./3.) + sqrt(abs(y)) * ln(x + 3)
+b =
