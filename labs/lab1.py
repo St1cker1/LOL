17c7f11a4d0f47532fa0986bc1c40279a02e981d
dmitry <avvakumov@gmail.com>
2023-09-27 11:17:38 +0500

    пристуапаю ко 2ому

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,12 +1,19 @@
 from math import *  #импортировал математику
 
-x = float(input("Введите x: "))
-y = float(input("Введите y: "))
-z = float(input("Введите z: "))
 
-a = log1p(abs(x-1)) + abs(y**(1./3.))/(1 + x**(1./3.)) + sin(y) * sin(x)
-b = (abs(x - 1))**(1./3.) + cos(z) * tan(4 * x) + sinh(y)
-print(a, b)
+def task1():
+    x = float(input("Введите x: "))
+    y = float(input("Введите y: "))
+    z = float(input("Введите z: "))
+
+    a = log1p(abs(x-1)) + abs(y**(1./3.))/(1 + x**(1./3.)) + sin(y) * sin(x)
+    b = (abs(x - 1))**(1./3.) + cos(z) * tan(4 * x) + sinh(y)
+    print(a, b)
 
 
+def task2():
+    pass
+
+
+task2()
 
