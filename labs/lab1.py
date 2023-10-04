3a9791dfea6948ebd0a8ff5710c83ed9dd5e36f3
dmitry <avvakumov@gmail.com>
2023-10-04 11:14:39 +0500

    приступаю к 4ому

--- labs/lab1.py
+++ labs/lab1.py
@@ -10,10 +10,23 @@ def task1():
     b = (abs(x - 1))**(1./3.) + cos(z) * tan(4 * x) + sinh(y)
     print(a, b)
 
-
 def task2():
-    pass
+    x = float(input("Введите x: "))
+    a = 3
+    b = 2
+    c = -2
+    f = x ** b + sqrt(a*x)/c*x + a
+    print(f)
+def task3():
+    x = float(input("Введите x: "))
+    f = tanh(x)**2 * (abs(log(3, x**2)))
+    print(f)
 
+def task4():
+    r = float(input("Введите: r "))
+    h = float(input("Введите: h "))
+    l = float(input("Введите: l"))
+    R =
 
-task2()
+task4()
 
