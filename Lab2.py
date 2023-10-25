0e7156791edf824ec0a760882fd10241bbef3822
St1cker1 <144201961+St1cker1@users.noreply.github.com>
2023-10-25 11:14:45 +0500

    Update Lab2.py
    :(

---
+++ Lab2.py
@@ -0,0 +1,25 @@
+from math import *
+
+import numpy as numpy
+
+
+def task1():
+    x = int(input('Введите число '))
+    if x % 2 == 0:
+        print("Оканчивается на четную цифру")
+    else:
+        print("Не оканчивается на четную цифру")
+
+    x = int(input("Введите число"))
+    if (sqrt(x)) % 1 == 0:
+            print('Квадратное число')
+    else:
+            print('Бесквадратное число')
+
+
+
+def task2():
+    a = int(input("Введите a "))
+    b = int(input("Введите b "))
+    for x in range(a, b)
+        (x - e)**2 and 7*sin(x) + 9*cos(x)
