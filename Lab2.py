1ad0253acec0451c301a93df7e8ba989bab93b6b
St1cker1 <144201961+St1cker1@users.noreply.github.com>
2023-12-13 11:09:11 +0500

    Update Lab2.py
    5 и 6 готово

--- Lab2.py
+++ Lab2.py
@@ -44,5 +44,52 @@ def task3():
         print(c)
         d = a // b
         a = d
+def task5():
+    def find_floor(n, k):
+    apartment_number = int(input("Введите номер квартиры: "))
 
-task2()
+    if apartment_number < 1 or apartment_number > n * k:
+        print("Ошибка! Неверный номер квартиры.")
+        return
+
+    floor = (apartment_number - 1) // k + 1
+
+    if floor % 3 == 0:
+        print("Лифт не может ехать на этот этаж.")
+        print("Выберите один из ближайших этажей:")
+        nearest_floors = [floor - 1, floor + 1]
+        for nearest_floor in nearest_floors:
+            if nearest_floor % 3 != 0:
+                print(nearest_floor)
+        return
+
+    print("Лифт едет на этаж:", floor)
+
+def task6():
+    def calculate_population(weeks, herbivores, predators):
+    for week in range(1, weeks + 1):
+        print("Неделя", week)
+        print("Травоядные:", herbivores)
+        print("Хищники:", predators)
+        
+        herbivores -= herbivores * 0.01  # Уменьшение популяции травоядных на 1% в месяц
+        predators -= predators * 0.03  # Уменьшение популяции хищников на 3% в месяц
+        
+        herbivores -= predators * 0.1  # Сокращение популяции травоядных на 10% от числа хищников
+        
+        if herbivores <= 0:
+            print("Популяция травоядных исчезла")
+            break
+        if predators <= 0:
+            print("Популяция хищников исчезла")
+            break
+
+weeks = int(input("Введите количество недель: "))
+herbivores = int(input("Введите начальное количество травоядных: "))
+predators = int(input("Введите начальное количество хищников: "))
+
+calculate_population(weeks, herbivores, predators)
+def task7():
+    pip install pandas
+    import pandas as pd
+    S = sin(x) +
